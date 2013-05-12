40c4a4e5a4278406c3e417035011931b9ef7c3e2
kimihito <tatsurotamashiro@gmail.com>
2013-05-12 09:16:10 +0900

    chapter3

--- scheme_in_ruby.rb
+++ scheme_in_ruby.rb
@@ -1,16 +1,45 @@
-def _eval(exp)
+def _eval(exp,env)
 	if not list?(exp)
 		if immediate_val?(exp)
 			exp
 		else
-			lookup_primitive_fun(exp)
+			lookup_var(exp,env)
 		end
 	else
-		fun = _eval(car(exp))
-		args = eval_list(cdr(exp))
-		apply(fun, args)
+		if special_form?(exp)
+			eval_special_form(exp, env)
+		else
+			fun = _eval(car(exp), env)
+			args = eval_list(cdr(exp), env)
+			apply(fun, args)
+		end
 	end
 end
+
+def special_form?(exp)
+	lambda?(exp) or 
+		let?(exp) or
+		letrec?(exp) or
+		if?(exp)
+end
+
+
+def lambda?(exp)
+	exp[0] == :lambda
+end
+
+def eval_special_form(exp, env)
+	if lambda?(exp)
+		eval_lambda(exp, env)
+	elsif let?(exp)
+		eval_let(exp, env)
+	elsif letrec?(exp)
+		eval_letrec(exp, env)
+	elsif if?(exp)
+		eval_if(exp, env)
+	end
+end
+
 
 def list?(exp)
 	exp.is_a?(Array)
@@ -24,7 +53,16 @@ $primitive_fun_env ={
 	:+ => [:prim, lambda {|x, y| x + y}],
 	:- => [:prim, lambda {|x, y| x - y}],
 	:* => [:prim, lambda {|x, y| x * y}],
+	:> => [:prim, lambda {|x, y| x > y}],
+	:>= => [:prim, lambda {|x, y| x >= y}],
+	:< => [:prim, lambda {|x, y| x < y}],
+	:<= => [:prim, lambda {|x, y| x <= y}],
+	:== => [:prim, lambda {|x, y| x == y}],
 }
+
+$boolean_env = {:true => true, :false => false}
+
+$global_env = [$primitive_fun_env, $boolean_env]
 
 def car(list)
 	list[0]
@@ -34,8 +72,8 @@ def cdr(list)
 	list[1..-1]
 end
 
-def eval_list(exp)
-	exp.map { |e| _eval(e)  }
+def eval_list(exp, env)
+	exp.map { |e| _eval(e, env)}
 end
 
 def immediate_val?(exp)
@@ -47,7 +85,15 @@ def num?(exp)
 end
 
 def apply(fun, args)
-	apply_primitive_fun(fun,args)
+	if primitive_fun?(fun)
+		apply_primitive_fun(fun,args)
+	else
+		lambda_apply(fun, args)
+	end
+end
+
+def primitive_fun?(exp)
+	exp[0] == :prim
 end
 
 def apply_primitive_fun(fun, args)
@@ -55,4 +101,103 @@ def apply_primitive_fun(fun, args)
 	fun_val.call(*args)
 end
 
-puts _eval([:+, [:+, 1, 2], 3])
+def lookup_var(var,env)
+	alist = env.find{|alist| alist.key?(var)}
+	if alist == nil
+		raise "couldn't find value to variables:'#{var}'"
+	end
+	alist[var]
+end
+
+def exntend_env(parameters, args, env)
+	alist = parameters.zip(args)
+	h = Hash.new
+	alist.each{|k, v| h[k] =v }
+	[h] + env
+end
+
+def eval_let(exp, env)
+	parameters, args, body = let_to_parameters_args_body(exp)
+	new_exp = [[:lambda, parameters, body]] + args
+	_eval(new_exp, env)
+end
+
+def let_to_parameters_args_body(exp)
+	[exp[1].map{|e| e[0]}, exp[1].map { |e| e[1] }, exp[2]]
+end
+
+def let?(exp)
+	exp[0] == :let
+end
+
+def letrec?(exp)
+	exp[0] == :letrec
+end
+
+def eval_letrec(exp, env)
+	parameters, args, body = letrec_to_parameters_args_body(exp)
+	tmp_env = Hash.new
+	parameters.each do |parameter|
+		tmp_env[parameter] = :dummy
+	end
+	ext_env = exntend_env(tmp_env.keys(), tmp_env.values(), env)
+	args_val = eval_list(args, ext_env)
+	set_extend_env!(parameters, args_val, ext_env)
+	new_exp = [[:lambda, parameters, body]] + args
+	_eval(new_exp,ext_env)
+end
+
+def set_extend_env!(parameters,args_val, ext_env)
+	parameters.zip(args_val).each do |parameter, arg_val|
+		ext_env[0][parameter] = arg_val
+	end
+end
+
+def letrec_to_parameters_args_body(exp)
+	let_to_parameters_args_body(exp)
+end
+
+def eval_lambda(exp, env)
+	make_closure(exp, env)
+end
+
+def eval_if(exp, env)
+	cond, true_clause, false_clause = if_to_cond_true_false(exp)
+	if _eval(cond,env)
+		_eval(true_clause, env)
+	else
+		_eval(false_clause,env)
+	end
+end
+
+def if_to_cond_true_false(exp)
+	[exp[1], exp[2], exp[3]]
+end
+
+def if?(exp)
+	exp[0] == :if
+end
+
+def make_closure(exp, env)
+	parameters, body = exp[1], exp[2]
+	[:closure, parameters, body, env]
+end
+
+def lambda_apply(closure, args)
+	parameters, body, env = closure_to_parameters_body_env(closure)
+	new_env = exntend_env(parameters, args, env)
+	_eval(body, new_env)
+end
+
+def closure_to_parameters_body_env(closure)
+	[closure[1], closure[2], closure[3]]
+end
+
+
+exp = [:letrec,
+				[[:fact,
+					[:lambda, [:n], [:if,[:<, :n, 1], 1, [:*, :n, [:fact, [:-, :n, 1]]]]]]],
+					[:fact, 3]]
+
+puts _eval(exp, $global_env)
+
